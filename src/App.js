1fa31297a83159b6209ce6dc142104d8580ad40d
Omkar Mukadam <1607.omkar@gmail.com>
2019-11-25 09:14:43 -0600

    pdf read attempt 3

--- src/App.js
+++ src/App.js
@@ -4,36 +4,25 @@ import './App.css';
 import { Document, Page } from "react-pdf";
 
 function App() {
-  state = { numPages: null, pageNumber: 1 };
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
   onDocumentLoadSuccess = ({ numPages }) => {
     this.setState({ numPages });
-  };
- goToPrevPage = () =>
-    this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
-  goToNextPage = () =>
-    this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+  }
 
   render() {
     const { pageNumber, numPages } = this.state;
     return (
       <div>
-        <nav>
-          <button onClick={this.goToPrevPage}>Prev</button>
-          <button onClick={this.goToNextPage}>Next</button>
-        </nav>
-
-        <div style={{ width: 600 }}>
-          <Document
-            file="src/example.pdf"
-            onLoadSuccess={this.onDocumentLoadSuccess}
-          >
-            <Page pageNumber={pageNumber} width={600} />
-          </Document>
-        </div>
-
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
+        <Document
+          file="src/example.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
       </div>
     );
   }
